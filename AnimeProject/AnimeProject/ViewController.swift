a9165759bfdd0730daabfd74fe0b224ca99eef70
Cristian <sanchezcristian991@gmail.com>
2021-10-04 21:24:37 -0500

    Project created in Storyboard (swift)

---
+++ AnimeProject/AnimeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeProject
+//
+//  Created by Cristian Sanchez on 4/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
